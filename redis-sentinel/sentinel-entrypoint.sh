5b49af1933b3eb4c81362f10f0f07ec39210567e
Hossein Zolfi <hossein.zolfi@gmail.com>
2023-04-10 11:07:53 +0330

    Configure Sentinel/Replicas port

--- redis-sentinel/sentinel-entrypoint.sh
+++ redis-sentinel/sentinel-entrypoint.sh
@@ -3,5 +3,7 @@
 sed -i "s/\$SENTINEL_QUORUM/$SENTINEL_QUORUM/g" /redis/sentinel.conf
 sed -i "s/\$SENTINEL_DOWN_AFTER/$SENTINEL_DOWN_AFTER/g" /redis/sentinel.conf
 sed -i "s/\$SENTINEL_FAILOVER/$SENTINEL_FAILOVER/g" /redis/sentinel.conf
- 
+sed -i "s/\$SENTINEL_MASTER/$SENTINEL_MASTER/g" /redis/sentinel.conf
+sed -i "s/\$SENTINEL_PORT/$SENTINEL_PORT/g" /redis/sentinel.conf
+
 redis-server /redis/sentinel.conf --sentinel
